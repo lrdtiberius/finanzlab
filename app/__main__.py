824f13c30719b3f3eba0ea8a7e639f860fdf35fd
lrdtiberius <hasel.traeume_0d@icloud.com>
2026-09-02 19:09:40 +0200

    Enable credit archive support

--- app/__main__.py
+++ app/__main__.py
@@ -1,3 +1,8 @@
 import os
-from .web import run
-run(int(os.environ.get("PORT","8798")))
+
+from . import web
+from .archive_support import install_repository_archive_support, install_web_archive_support
+
+install_repository_archive_support(web.Repository)
+install_web_archive_support(web.Handler)
+web.run(int(os.environ.get("PORT", "8798")))
